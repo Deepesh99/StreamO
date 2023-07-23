8582bf3460f35a3734bf72e7fb34b959f4bab8fb
Deepesh <deepesh99.nair@gmail.com>
2023-07-23 20:55:54 +0530

    video metadata upload to database logic created

--- controller/video.js
+++ controller/video.js
@@ -1,10 +1,16 @@
 const Video = require('../model/video');
 
 exports.videoUpload = async(req, res) => {
-    // const { title } = req.body;
-    console.log(req.file);
-
-    return res.status(200).json({ status: true, message: 'Video Uploaded' });
+    const { title } = req.body;
+    
+    try {
+        await Video.create({title: title, url: req.file.path, userid: req.headers.userid});
+        return res.status(200).json({ status: true, message: 'Video Uploaded' });
+    }
+    catch(err) {
+        console.log(err);
+    }
+    
 }
 
 exports.video = async(req, res) => {
