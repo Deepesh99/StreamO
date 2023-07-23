c2ba58f8b3a4959cf2fffedffa46c5f779960663
Deepesh <deepesh99.nair@gmail.com>
2023-07-23 13:46:13 +0530

    created model for video db

---
+++ model/video.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+// user schema
+const videoSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+  userid: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
+  },
+});
+
+module.exports = mongoose.model('Video', videoSchema);
