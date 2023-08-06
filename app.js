c3d8b8670145f20c3bc12212fe2983aa41461927
Deepesh <deepesh99.nair@gmail.com>
2023-08-06 23:42:30 +0530

    chore: remove useless code

--- app.js
+++ app.js
@@ -1,48 +1,28 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 
 const db = require('../StreamO/utils/db');
-require('dotenv').config();
 const app = express();
 
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false}));
+
+// cors
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', '*');
     next();
 });
+
 // import routes
 const authRoutes = require('./routes/auth');
 const generalRoutes = require('./routes/general');
-app.use(bodyParser.json());
-app.use( express.urlencoded({ extended: false}));
+
 // routing
 app.use('/auth', authRoutes);
 app.use('/user', generalRoutes);
-app.get('/', (req, res)=>{
-    res.sendFile(__dirname + '/index.html');
-});
-
-// app.get('/videoplayer', (req, res) => {
-//     const range = req.headers.range;
-//     const videoPath = './video.mp4';
-//     const videoSize = fs.statSync(videoPath).size;
-//     const chunkSize = 1 * 1e6;
-//     const start = Number(range.replace(/\D/g, ""));
-//     const end = Math.min(start + chunkSize, videoSize - 1);
-//     const videoLength = end - start + 1;
-//     const headers = {
-//         "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-//         "Accept-Ranges": "bytes",
-//         "Content-Length": videoLength,
-//         "Content-Type": "video/mp4"
-// }
- 
-// res.writeHead(206, headers)
-// const stream = fs.createReadStream(videoPath, { start, end })
-// stream.pipe(res)
-// })
 
 app.listen(process.env.PORT);
-
